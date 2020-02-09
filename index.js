06b52b802fdbbcbf96ed2a3f2dabe08a2549a8fe
seonju <seonju1022@gmail.com>
2020-02-09 23:14:15 +0900

    git update index.js - add app.use() method

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require("express"); // npm으로 설치한 Express module을 불러와서 변수(express)에 담는다
 var app = express(); // express 실행 - app object 초기화
 
-app.get('/', function(req, res) { // '/' 경로에 'get' 방식의 요청을 받을 때,
-    res.send("hi"); // 왼쪽 코드("hi")를 보낸다
-});
+app.use(express.static(__dirname + "/public")); // 1
 
 var port = 3000; // 포트 번호
 app.listen(port, function() { // 변수(port)를 이용해 3000번 포트에 node.js 서버를 연결
